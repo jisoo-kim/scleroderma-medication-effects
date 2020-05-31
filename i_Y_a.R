88944122737c3e6e3934e71c6cbe9eeab053a208
yizhenxu <yizhen_xu@brown.edu>
2020-05-31 12:46:08 -0400

    update plot scripts

--- i_Y_a.R
+++ i_Y_a.R
@@ -1,12 +1,11 @@
-load("Data.RData") #load d
+load("tmp_plot.RData") #load d
 
 ### Plot of (pFVC, -mRSS, EF) above and (Pred, MTX, MMF, CTX)
 mv = c("Pred","MTX","MMF","CTX","IVIG","AZA","Rituximab","Tocilizumab","HCQ","TNF","LEF")
-dat = d[, c("Patient.ID","YTime","FVC", "DLCO", "RVSP", "EF", "mRSS", mv), with=F]
+dat = d[, c("Patient.ID","Time","FVC", "DLCO", "RVSP", "EF", "mRSS", mv, "med"), with=F]
 colnames(dat)[7] = "nmRSS"; dat[,7] = -dat[,7]
-dat = dat[order(Patient.ID, YTime),]
+dat = dat[order(Patient.ID, Time),]
 
-if(randomID) idlist = sample(unique(dat$Patient.ID),4)
 #length(unique(dat$Patient.ID)) # 1211 unique patients
 
 dat$nonMMF = apply(dat[,mv[-3],with=F],1,function(x) 1*(sum(x)!=0) )# indicator of on trt (excluding MMF)
@@ -15,11 +14,13 @@ if(trt_type == 1){
   nl = 1:4
   levlist = c("no trt","MMF","MMF+","others")
   dat[, type := 1] # no trt
-  dat[MMF == 1 & nonMMF == 0, type := 2] # 2 MMF only
+  #dat[MMF == 1 & nonMMF == 0, type := 2] # 2 MMF only
   dat[MMF == 1 & nonMMF == 1, type := 3] # 3 MMF+
   dat[MMF == 0 & nonMMF == 1 , type := 4] # 4 only others
+  dat[MMF == 1 & nonMMF == 0, type := 2] 
+  dat[MMF == 1 & HCQ == 1, type := 2] # 2 MMF only OR MMF + HCQ
+  
 }
-
 if(trt_type == 2){
   nl = 1:5
   levlist = c("no trt","MMF","MMF+","others", "Pred")
@@ -31,18 +32,29 @@ if(trt_type == 2){
   dat[MMF == 0 & Pred == 0 & nonPred+nonMMF >= 1 , type := 4] # 4 only others (non Pred or MMF)
   
 }
+dat[med==0, type := NA]
 
 # ind is 1 if FVC or mRSS  or EF does not have any measurement
-dat[, ind := 1*(sum(!is.na(FVC[1:.N]))*sum(!is.na(nmRSS[1:.N]))*sum(!is.na(EF[1:.N])) ==0), by = Patient.ID]
+#dat[, ind := 1*(sum(!is.na(FVC[1:.N]))*sum(!is.na(nmRSS[1:.N]))*sum(!is.na(EF[1:.N])) ==0), by = Patient.ID]
+dat[, ind := 1*(sum(!is.na(FVC[1:.N]))*sum(!is.na(nmRSS[1:.N])) ==0), by = Patient.ID]
+
 dat = dat[ind==0,]
+
+#idlist = c(2087,1399,3382,2646)
+if(randomID) idlist = sample(unique(dat$Patient.ID),4)
+
+
+tmp = dat[Patient.ID %in% idlist, ]
+rg1 = range(tmp$FVC, na.rm=T)
+rg2 = range(tmp$nmRSS, na.rm=T)
 
 layout(matrix(c(rep(1,3),2,rep(3,3),4,rep(5,3),6,rep(7,3),8), nrow = 8, ncol = 2))
 
 for(id in idlist){
   pd = dat[Patient.ID == id,]
-  ind1 = which(!is.na(pd$FVC)); timeFVC = pd$YTime[ind1]; FVC = pd$FVC[ind1]
-  ind2 = which(!is.na(pd$nmRSS)); timeRSS = pd$YTime[ind2]; RSS = pd$nmRSS[ind2]
-  ind3 = which(!is.na(pd$EF)); timeEF = pd$YTime[ind3]; EF = pd$EF[ind3]
+  ind1 = which(!is.na(pd$FVC)); timeFVC = pd$Time[ind1]; FVC = pd$FVC[ind1]
+  ind2 = which(!is.na(pd$nmRSS)); timeRSS = pd$Time[ind2]; RSS = pd$nmRSS[ind2]
+  #ind3 = which(!is.na(pd$EF)); timeEF = pd$Time[ind3]; EF = pd$EF[ind3]
   
   rgt = range(c(timeFVC,timeRSS))
   
@@ -50,7 +62,7 @@ for(id in idlist){
   par(mar=c(0,4,3,4))
   
   ## Plot first set of data and draw its axis
-  plot(timeFVC, FVC, pch=16, axes=FALSE, ylim = range(FVC), xlab="", ylab="", xlim = rgt,
+  plot(timeFVC, FVC, pch=16, axes=FALSE, ylim = rg1, xlab="", ylab="", xlim = rgt,
        type="b",col="black", main=paste0("ID ", id))
   axis(2, ylim=range(FVC),col="black",las=1)  ## las=1 makes horizontal labels
   mtext("FVC",side=2,line=2.5)
@@ -60,7 +72,7 @@ for(id in idlist){
   par(new=TRUE)
   
   ## Plot the second plot and put axis scale on right
-  plot(timeRSS, RSS, pch=15,  xlab="", ylab="", ylim=range(RSS), xlim = rgt,
+  plot(timeRSS, RSS, pch=15,  xlab="", ylab="", ylim=rg2, xlim = rgt,
        axes=FALSE, type="b", col="red")
   ## a little farther out (line=4) to make room for labels
   mtext("-mRSS",side=4,col="red",line=2.5) 
@@ -79,10 +91,12 @@ for(id in idlist){
   #legend("topright",legend=c("pFVC","mRSS"),
   #       text.col=c("black","red"),pch=c(16,15),col=c("black","red"),cex=1.5)
   
-  par(mar=c(4,4,0.5,4))
+  #par(mar=c(4,4,0.5,4))
+  #par(mar=c(3.5,4,0,4))
+  par(mar=c(4,4,0,4))
   ## Medication
   
-  ind = which(!is.na(pd$type)); t = pd$YTime[ind]; M = pd$type[ind]
+  ind = which(!is.na(pd$type)); t = pd$Time[ind]; M = pd$type[ind]
   
   plot(t, rep(0.5, length(t)), xlab="", ylab="",xlim = rgt, ylim = c(0,1),
        main="", col = M, pch = 19, axes=FALSE)
@@ -90,3 +104,5 @@ for(id in idlist){
   axis(1,pretty(rgt,10))
   mtext("Time (Years since onset)",side=1,col="black",line=2.5)  
 }
+
+
--- p_A_dotplot.R
+++ p_A_dotplot.R
@@ -1,17 +1,10 @@
-load("Data.RData") #load d
+load("tmp_plot.RData") #load d
 
-# Define length of follow-up
-if(onset == 1){
-  d$YTime = d$YTime1
-} else if(onset == 2){
-  d$YTime = d$YTime0
-}
-
-d[, fup := YTime[.N], by = Patient.ID]
-d[, sup := YTime[1], by = Patient.ID]
+d[, fup := Time[.N], by = Patient.ID]
+d[, sup := Time[1], by = Patient.ID]
 
 mv = c("Pred","MTX","MMF","CTX","IVIG","AZA","Rituximab","Tocilizumab","HCQ","TNF","LEF")
-pd = d[!is.na(med) & med == 1, c("Patient.ID", "YTime", "fup", "sup", mv),with=F]
+pd = d[med == 1, c("Patient.ID", "Time", "fup", "sup", mv),with=F]
 
 pd$nonMMF = apply(pd[,mv[-3],with=F],1,function(x) 1*(sum(x)!=0) )# indicator of on trt (excluding MMF)
 
@@ -19,9 +12,12 @@ if(trt_type == 1){
   nl = 1:4
   levlist = c("no trt","MMF","MMF+","others")
   pd[, type := 1] # no trt
-  pd[MMF == 1 & nonMMF == 0, type := 2] # 2 MMF only
+  #pd[MMF == 1 & nonMMF == 0, type := 2] # 2 MMF only
   pd[MMF == 1 & nonMMF == 1, type := 3] # 3 MMF+
   pd[MMF == 0 & nonMMF == 1 , type := 4] # 4 only others
+  pd[MMF == 1 & nonMMF == 0, type := 2] 
+  pd[MMF == 1 & HCQ == 1, type := 2] # 2 MMF only OR MMF + HCQ
+  
 }
 
 if(trt_type == 2){
@@ -36,17 +32,18 @@ if(trt_type == 2){
   
 }
 
-pd = pd[, c("Patient.ID", "YTime", "fup", "sup", "type"), with=F]
+pd = pd[, c("Patient.ID", "Time", "fup", "sup", "type"), with=F]
 
 # nid number of patients into two columns of plots
 nid = 100; pncol = 2
 # Randomize nid number of patients and reassign IDs as 1 to nid
+set.seed(80)
 idlist = sample(unique(pd$Patient.ID), nid)
 pdall = pd[Patient.ID %in% idlist, ]
 len =  pdall[,.N, by = Patient.ID]$N; pdall[,id:= rep(1:nid, len)]
 
 m = matrix(c(1,2,3,3),nrow = 2,ncol = pncol,byrow = TRUE)
-layout(mat = m,heights = c(0.9,0.1))
+layout(mat = m,heights = c(0.9,0.15))
 for( i in 1:pncol ){
   if(i==1) par(mai=c(0,0,0,0.2))
   if(i==2) par(mai=c(0,0,0,0.2))
@@ -55,7 +52,7 @@ for( i in 1:pncol ){
   rg = ln:(ln+nn)
   pd = pdall[id %in% rg, ]; pdfup = unique(pd[, c("id", "fup", "sup", "Patient.ID"), with=F])
   
-  plot(pd$YTime, pd$id, xlab="", ylab="",  yaxt='n', axes = F, xlim = c(0,max(pd$YTime,na.rm=T)+4),
+  plot(pd$Time, pd$id, xlab="", ylab="",  yaxt='n', axes = F, xlim = c(0,max(pd$Time,na.rm=T)+4),
        main="", col = pd$type, pch = 19, panel.first=for(k in 1:nrow(pdfup))  lines(c(pdfup[k,3], pdfup[k,2]), rep(pdfup[k,1],2), type = "l", lty = 1,col = rgb(red = 190/255, green =190/255, blue = 190/255)))
   #axis(2, at = unique(pd$id),labels =as.character(unique(pd$Patient.ID)), las = 2, cex.axis = 0.5)
   axis(1)
